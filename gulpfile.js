84e1850690d2291f57fa4fd591623df6fbab5fba
Shane Rogers <rogerssh@tcd.ie>
2015-08-02 02:02:21 -0700

    Remove AWS from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,25 +8,14 @@ var csso = require('gulp-csso');
 var rename = require('gulp-rename');
 var fs = require('fs');
 var minimist = require('minimist');
-
-// AWS Publishing
-var awsCreds = JSON.parse(fs.readFileSync('./aws.json'));
-var TWO_YEAR_CACHE_PERIOD_IN_SEC = 60 * 60 * 24 * 365 * 2;
-var awspublish = require('gulp-awspublish');
-var publisher = awspublish.create(awsCreds);
-var headers = {
-  'Cache-Control': TWO_YEAR_CACHE_PERIOD_IN_SEC + "",
-  'Expires': new Date('2030')
-};
 var knownOptions = {
   string: 'env',
   default: { env: process.env.NODE_ENV || 'production' }
 };
 var options = minimist(process.argv.slice(2), knownOptions);
-// options.env
 
 gulp.task('clean', function () {
-  del(['./temp/js/*.js'], function (err, paths) {
+  del(['./temp/js/*.js', './temp/css/*.css'], function (err, paths) {
     gutil.log(
       'Deleted files/folders:\n',
       gutil.colors.cyan(paths.join('\n'))
@@ -47,24 +36,26 @@ function run(command) {
 }
 
 gulp.task('less', function() {
-  var location = options.location === 'user' ? 'temp' : 'public';
+  var destPrefix = options.location === 'user' ? 'temp' : 'public';
+  var dest = [destPrefix, '/css'].join('');
 
-  gulp.src(location+ '/less/main.less')
+  // Initial src is always the main public (until separate sheets are used)
+  gulp.src('public/less/main.less')
     .pipe(sourcemaps.init())
     .pipe(less())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(location+'/css'));
+    .pipe(gulp.dest(dest));
 });
 
 gulp.task('cssmin', function() {
-  var location = options.location === 'user' ? 'temp' : 'public';
+  var destPrefix = options.location === 'user' ? 'temp' : 'public';
 
-  gulp.src(location + '/css/main.css')
+  gulp.src(destPrefix+'/css/main.css')
     .pipe(csso())
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest(location + 'public/css'));
+    .pipe(gulp.dest(destPrefix+'/css'));
 });
 
 gulp.task('watch', function() {
@@ -80,39 +71,3 @@ gulp.task('dev', ['default'], function() {
 gulp.task('css', ['less', 'cssmin']);
 
 gulp.task('default', ['less', 'watch']);
-
-gulp.task('mayk', ['less', 'watch']);
-
-gulp.task('publishAssets', ['publishJS', 'publishCSS']);
-
-gulp.task('publishCSS', ['less', 'cssmin'], function() {
-  var s3Dirname = ['/',options.username,'/css'].join('');
-  // Will take the min.css file and push it to S3
-  return gulp.src('./temp/css/*.css')
-    .pipe(rename(function (path) {
-        path.dirname += s3Dirname
-    }))
-    .pipe(publisher.publish(headers))
-    .pipe(publisher.cache())
-    .pipe(awspublish.reporter());
-})
-
-gulp.task('publishJS', function() {
-  var s3Dirname = ['/',options.username,'/js'].join('');
-  // Will take the min.js file and push it to S3
-  return gulp.src('./temp/js/*.js')
-    .pipe(rename(function (path) {
-        path.dirname += s3Dirname
-    }))
-    .pipe(publisher.publish(headers))
-    .pipe(publisher.cache())
-    .pipe(awspublish.reporter());
-});
-
-gulp.task('jsGzip', function() {
-  return gulp.src('./temp/js/*.js')
-    .pipe(awspublish.gzip({ ext: '.gz' }))// gzip, Set Content-Encoding headers and add .gz extension
-    .pipe(publisher.publish(headers))
-    .pipe(publisher.cache())
-    .pipe(awspublish.reporter());
-});
